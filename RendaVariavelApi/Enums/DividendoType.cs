2ce3da426f6626183c577c11a4ce957340993b69
iaguuu <iagooliveiraviana@gmail.com>
2024-04-09 21:32:50 -0300

    Enumerators

---
+++ RendaVariavelApi/Enums/DividendoType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace RendaVariavelApi.Enums
+{
+    public enum DividendoType
+    {
+        [Description("JUROS SOBRE CAPITAL PROPIO")]
+        JCP = 1,
+        [Description("DIVIDENDOS")]
+        DIVIDENDOS = 2,
+        [Description("AMORTIZACAO")]
+        AMORTIZACAO = 3
+    }
+}
---
+++ RendaVariavelApi/Enums/InvestimentoType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace RendaVariavelApi.Enums
+{
+    public enum InvestimentoType
+    {
+        [Description("FUNDO IMOBILIARIO")]
+        FII,
+        [Description("FUNDO DE INVESTIMENTO EM INFRAESTRUTURA ")]
+        FII_INFRA,
+        [Description("FUNDO DE INVESTIMENTO EM CADEIAS AGROINDÚSTRIAIS")]
+        FII_AGRO,
+        [Description("ACOES")]
+        ACOES,
+        [Description("BDRS")]
+        BDRS,
+        [Description("NAO DEFINIDO")]
+        NAO_DEFINIDO
+    }
+}
